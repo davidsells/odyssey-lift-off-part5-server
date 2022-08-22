2c6650510adc96fa2748d75f19979944d613c19e
David Persistent Designs <david@persistentdesigns.com>
2022-08-22 08:48:49 -0400

    kick to server

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ async function startApolloServer(typeDefs, resolvers) {
   const { url, port } = await server.listen({port: process.env.PORT || 4000});
 
   console.log(`
-      🚀  Server is running
+      🚀  Server is running hot
       🔉  Listening on port ${port}
       📭  Query at ${url}
     `);
